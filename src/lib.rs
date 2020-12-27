d22d198d9faa92a9672601ae19b2794533da6f1e
ddlin.macMini <499070844@163.com>
2020-12-27 23:03:02 +0800

    We can create a board now, but still have a bug in Minewreeper::get_block_idx()

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 //! 4. 反转后还要在上面显示数字
 //! 5. 如何连带其他格子反转
 
-use std::fmt::*;
+use std::{collections::HashMap, fmt::*};
 
 struct Minewreeper {
     /// 一共81个
@@ -20,7 +20,7 @@ struct Minewreeper {
     ///
     /// `is_wine` == true
     is_win: bool,
-    block: Vec<Block>,
+    blocks: Vec<Block>,
 }
 
 impl Minewreeper {
@@ -30,40 +30,86 @@ impl Minewreeper {
             total: 81,
             last: 10,
             is_win: false,
-            block: Vec::new(),
+            blocks: Vec::new(),
         };
 
-     for y1 in (0..9).rev() {
-             for x1 in 0..9 {
+        for y1 in (0..9).rev() {
+            for x1 in 0..9 {
                 let position = (x1, y1);
                 let mut is_boom = false;
+                let index = minewreeper.blocks.len();
                 for pos in &mine_pos {
                     is_boom = *pos == position;
-                    if is_boom { break; }
+                    if is_boom {
+                        break;
+                    }
                 }
                 if is_boom {
-                    minewreeper.block.push(Block::new(position, is_boom));
+                    minewreeper
+                        .blocks
+                        .push(Block::new(position, is_boom, index));
                 } else {
-                    minewreeper.block.push(Block::new(position, is_boom));
+                    minewreeper
+                        .blocks
+                        .push(Block::new(position, is_boom, index));
                 }
             }
         }
-        println!("init success");
         minewreeper
     }
 
-    pub fn is_init(&self) -> bool { self.block.len() > 0 }
+    pub fn is_init(&self) -> bool {
+        self.blocks.len() > 0
+    }
+
+    pub fn crutalmovment(&mut self) {
+        let mut unsafe_blocks = Vec::new();
+        for b in &self.blocks {
+            if b.is_boom {
+                let cur_pos = &b.point;
+                let e = Minewreeper::get_block_idx(&(cur_pos.0 + 1, cur_pos.1));
+                let s = Minewreeper::get_block_idx(&(cur_pos.0, cur_pos.1 - 1));
+                let n = Minewreeper::get_block_idx(&(cur_pos.0, cur_pos.1 + 1));
+                let w = Minewreeper::get_block_idx(&(cur_pos.0 - 1, cur_pos.1));
+                let en = Minewreeper::get_block_idx(&(cur_pos.0 + 1, cur_pos.1 + 1));
+                let es = Minewreeper::get_block_idx(&(cur_pos.0 + 1, cur_pos.1 - 1));
+                let wn = Minewreeper::get_block_idx(&(cur_pos.0 - 1, cur_pos.1 + 1));
+                let ws = Minewreeper::get_block_idx(&(cur_pos.0 - 1, cur_pos.1 - 1));
+                unsafe_blocks.push(e);
+                unsafe_blocks.push(s);
+                unsafe_blocks.push(n);
+                unsafe_blocks.push(w);
+                unsafe_blocks.push(en);
+                unsafe_blocks.push(es);
+                unsafe_blocks.push(wn);
+                unsafe_blocks.push(ws);
+            }
+        }
+        for unsafe_idx in unsafe_blocks {
+            if let Some(idx) = unsafe_idx {
+                self.blocks[idx].text += 1;
+            }
+        }
+    }
+
+    pub fn get_block_idx(pos: &(u8, u8)) -> Option<usize> {
+        println!("{:?}", pos);
+        if pos.0 < 0 || pos.0 > 8 { return None }
+        if pos.1 < 0 || pos.1 > 8 { return None }
+        Some((pos.0 + (8 - pos.1) * 9) as usize)
+    }
 }
 
 impl Display for Minewreeper {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         let mut result = Vec::new();
-        for i in 0..self.block.len() {
-            result.push(format!("{}", self.block[i]));
+        for i in 0..self.blocks.len() {
+            result.push(format!("{}", self.blocks[i]));
         }
-        println!("{}",result.len());
         for (count, res) in result.iter().enumerate() {
-            if count % 9 == 0 { write!(f, "\r\n")?; }
+            if count % 9 == 0 {
+                write!(f, "\r\n")?;
+            }
             write!(f, "{} ", res)?;
         }
         write!(f, "\r\n")
@@ -73,44 +119,36 @@ impl Display for Minewreeper {
 struct Block {
     /// (x, y)
     ///
-    /// x <= 9
+    /// x <= 8
     ///
-    /// y <= 9
+    /// y <= 8
     point: (u8, u8),
     flag: BlockFlag,
     is_boom: bool,
     // near_block: [NearBlock; 8],
     text: u8,
 }
-enum NearBlock {
-    E(Option<Box<Block>>),
-    N(Option<Box<Block>>),
-    W(Option<Box<Block>>),
-    S(Option<Box<Block>>),
-    EN(Option<Box<Block>>),
-    ES(Option<Box<Block>>),
-    WN(Option<Box<Block>>),
-    WS(Option<Box<Block>>),
-}
 
 impl Block {
-    pub fn new(position: (u8, u8), is_boom: bool) -> Self {
+    pub fn new(position: (u8, u8), is_boom: bool, index: usize) -> Self {
         Self {
             point: position,
             flag: BlockFlag::Normal,
             is_boom,
             text: 0,
+            // near_block: test,
         }
     }
 
     pub fn set_text(&mut self, t: u8) {
-        if t > 8 { 
+        if t > 8 {
             println!("error! text only small than 9");
-            std::process::exit(101); 
+            std::process::exit(101);
         }
         self.text = t;
     }
 }
+
 
 impl Display for Block {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
@@ -128,10 +166,9 @@ enum BlockFlag {
     Marked,
 }
 
-
 #[cfg(test)]
 mod tests {
-    use rand::{ thread_rng, Rng };
+    use rand::{thread_rng, Rng};
 
     use crate::Minewreeper;
 
@@ -144,6 +181,10 @@ mod tests {
             let y: u8 = rng.gen_range(0..9);
             mine.push((x, y));
         }
-        print!("{}", Minewreeper::init(mine))
+        let mut board = Minewreeper::init(mine);
+        print!("{}", board);
+        print!("\r\n");
+        board.crutalmovment();
+        println!("{}", board);
     }
 }
